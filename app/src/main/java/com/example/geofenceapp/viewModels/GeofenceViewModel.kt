94c45dc8d7e5bb57a79a50a30b2a3a53aa084298
Joel Parizi <joelparizi@Joels-MBP.attlocal.net>
2021-02-25 13:21:06 -0500

    Setup and architecture complete

--- app/src/main/java/com/example/geofenceapp/viewModels/GeofenceViewModel.kt
+++ app/src/main/java/com/example/geofenceapp/viewModels/GeofenceViewModel.kt
@@ -1,8 +1,49 @@
 package com.example.geofenceapp.viewModels
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.geofenceapp.model.UserData
+import com.example.geofenceapp.network.remote.GeofenceRepository
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
+import org.koin.core.inject
 
+private const val TAG = "GeofenceViewModel"
 class GeofenceViewModel : ViewModel(), KoinComponent {
 
+    private val service: GeofenceRepository by inject()
+
+    var responseError = ""
+    val userMutableLiveData = MutableLiveData<UserData>()
+    val userLiveData: LiveData<UserData>
+        get() = userMutableLiveData
+    private var job: Job? = null
+
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
+        //Cancels job when ViewModel is terminated
+    }
+
+
+    fun basicCoroutineFetch() {
+
+        viewModelScope.launch {
+            val response = service.getUserUpdate(userMutableLiveData.value.toString())
+            if (response.isSuccessful && response.body() != null) {
+                userMutableLiveData.value = response.body()
+            } else {
+                responseError = response.errorBody().toString()
+                onError(responseError)
+            }
+        }
+    }
+
+    private fun onError(message: String) {
+        Log.d(TAG, "onError: $message")
+    }
 }
